01ed51ce45ace5b05288a591b158240cb0905a31
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-12-15 21:27:38 +0200

    feat: users email and password sign up with firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,11 @@
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
+import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
